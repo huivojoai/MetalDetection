5568201f50b57d3e2486f854f9312ad940dab77e
huivojoai <79126712+huivojoai@users.noreply.github.com>
2024-08-10 23:48:45 -0400

    Update detect.py

--- detect.py
+++ detect.py
@@ -23,7 +23,7 @@ def main():
 
         # ========================== Update for each model: 
         n = 1
-        probability_threshold = 50
+        probability_threshold = 10
 
 
         # Load image and get height, width and channels
