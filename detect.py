e1fb6bd4c8ad3d2d49d6788621fe9ed34c7028f2
huivojoai <79126712+huivojoai@users.noreply.github.com>
2024-08-11 00:25:15 -0400

    Update detect.py

--- detect.py
+++ detect.py
@@ -71,12 +71,7 @@ def main():
                         draw.line(points, fill=color, width=lineWidth)
                         # Add the tag name and probability
                         plt.annotate(prediction.tag_name + ": {0:.2f}%".format(prediction.probability * 100),(left,top), backgroundcolor=color, fontsize=324)
-                # buf = io.BytesIO()
-                # fig.savefig(buf)
-                # buf.seek(0)
-                output_image = Image.open(image)
-                
-                st.image(image, caption="Detected Image", use_container_width=True)
+                st.image(image, caption="Detected Output", use_column_width=True)
                 
             
             # # outputfile = f'res_part_{i}.jpg'
